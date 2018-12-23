859886cb488d5cc7df37020b24d25705a8635e20
msdnqqy <545664719@qq.com>
2018-12-23 13:50:30 +0800

    更新key

--- Sarsa_maze/RL_brain.py
+++ Sarsa_maze/RL_brain.py
@@ -24,3 +24,31 @@ class RL(object):
     def check_state_exist(self,observation):
         if observation not in self.q_table.index:
             q_table.loc[observation]=[0]*len(self.actionsk)
+
+
+class SarsaTable(RL):
+    def __init__(self,actions=['right','left','up','down']):
+        super(SarsaTable,self).__init__(actions=actions)
+        self.other_q_table=self.q_table.copy()
+        self.lambda_=self.gamma
+    
+    def check_state_exist(self,observation):
+        if observation not in self.q_table.index:
+            self.q_table.loc[observation]=[0]*len(self.actions)
+            self.other_q_table.loc[observation]=[0]*len(self.actions)
+
+    def learn(self,observation,action,reward,observation_next,action_next,is_success):
+        self.check_state_exist(observation_next)
+        q_predict=self.q_table.loc[observation,action]
+
+        if is_success:
+            q_real=reward
+        else:
+            q_real=reward+self.gamma*self.q_table.loc[observation_next,action_next]
+
+        error=self.lr*(q_real-q_predict)
+
+        self.other_q_table.loc[observation,:]=0
+        self.other_q_table.loc[observation,action]=1
+        self.q_table+=error*self.other_q_table
+        self.other_q_table*=self.gamma*self.lambda_
---
+++ gym_test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ~/.mujoco/mjpro150/bin
+./simulate ../model/humanoid.xml
+
